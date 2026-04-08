53f20acaa8687b7fdded101b747462732c56b3bb
slyeet03 <shobhit7419@gmail.com>
2026-04-09 03:11:27 +0530

    made the discovery faster

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.slyeet.linkee"
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
